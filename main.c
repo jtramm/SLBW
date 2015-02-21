4e080032dcc90b3431d81a45dae33e222a0be319
John Tramm <john.tramm@gmail.com>
2015-02-21 14:44:33 -0600

    working!

--- main.c
+++ main.c
@@ -37,6 +37,9 @@ int main(void)
 	// 4 pi a^2
 	double sigma_pot = 11.29; // barns
 
+	double k = 8.6173324e-5;
+	double temp = 1000;
+
 	double * E = (double *) calloc( 4 * n_gridpoints, sizeof(double));
 	double * sigma_f = E + n_gridpoints; 
 	double * sigma_n = E + 2 * n_gridpoints;
@@ -45,35 +48,36 @@ int main(void)
 	// Initialize E to log scale (10^-2 <-> 10^2)
 	for( int i = 0; i < n_gridpoints; i++ )
 	{
-		double delta = 4.f / n_gridpoints;
-		E[i] = pow(10.f,-2.f + delta*i);
+		double delta = 4.0 / n_gridpoints;
+		E[i] = pow(10.0,-2.0 + delta*i);
 	}
 
 	// Calculate sigmas
 	for( int i = 0; i < n_gridpoints; i++ )
 	{
-		double r = 2603911 / E[i] * 239 / 238;
-		double q = 2.f * sqrt(r * sigma_pot);
+		double r = 2603911.0 / E[i] * 239.0 / 238.0;
+		double q = 2.0 * sqrt(r * sigma_pot);
 
 		// Accumulate Contributions from Each Resonance
 		for( int j = 0; j < 3; j++ )
 		{
 			double T = R[j].Tn + R[j].Tg;
-			double x = 2.f * (E[i] - R[j].Eo) / T;
+			double x = 2.0 * (E[i] - R[j].Eo) / T;
+			double psi, chi;
 			if( temperature_dependent )
 			{
-				double k = 8.6173324e-5;
-				double temp = 1000;
-				double xi = T * sqrt(238.f / (4.f * k * temp * R[j].Eo));
-				double psi = sqrt(M_PI) * creal( xi * Faddeeva_w( (x + I) * xi, 0.f) ); 
+				double xi = T * sqrt(238.0 / (4.0 * k * temp * R[j].Eo));
+				double complex faddeeva_output = xi * Faddeeva_w( (x + I) * xi, 0.0);
+				psi = sqrt(M_PI) * creal(faddeeva_output); 
+				chi = sqrt(M_PI) * cimag(faddeeva_output);
 			}
 			else
 			{
-				double psi = 1 / (1 + x*x);
-				double chi = x / (1 + x*x);
-				sigma_f[i] += R[j].Tn * R[j].Tg / (T*T) * sqrt(R[j].Eo / E[i]) * r * psi;
-				sigma_n[i] += R[j].Tn * R[j].Tg / (T*T) * ( r * psi + q * chi ) + sigma_pot; 
+				psi = 1.0 / (1.0 + x*x);
+				chi = x / (1.0 + x*x);
 			}
+			sigma_f[i] += R[j].Tn * R[j].Tg / (T*T) * sqrt(R[j].Eo / E[i]) * r * psi;
+			sigma_n[i] += R[j].Tn * R[j].Tg / (T*T) * ( r * psi + q * chi ) + sigma_pot; 
 		}
 
 		// Calculate Total XS
