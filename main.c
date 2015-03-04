16ce6dce4b8810f569f4a4cd0f944c7c611cd124
John Tramm <john.tramm@gmail.com>
2015-03-04 17:44:23 -0500

    modularized code

--- main.c
+++ main.c
@@ -14,87 +14,73 @@ typedef struct{
 	double Tg;
 } Resonance;
 
+typedef struct{
+	double E;
+	double sigma_g;
+	double sigma_n;
+	double sigma_t;
+} XS;
+
 Resonance * res_read(int * n_resonances);
 double complex FNF( double complex Z );
 
-int main(void)
+XS calculate_XS( double E, Resonance * R, int nr )
 {
-	int temperature_dependent = 1;
-	int n_gridpoints = 10000;
-
-	int n_resonances;
-	Resonance * R = res_read(&n_resonances);
-
-	// Setting it to only graph first 3 resonances
-	//n_resonances = 3;
-
-	// 4 pi a^2
-	double sigma_pot = 11.2934; // barns
-
+	double sigma_pot = 11.2934;
 	double k = 8.6173324e-5;
 	double temp = 0.00001;
 	double A = 238.05078826;
+	XS xs = {0};
+	xs.E = E;
 
-	double * E = (double *) calloc( 4 * n_gridpoints, sizeof(double));
-	double * sigma_g = E + n_gridpoints; 
-	double * sigma_n = E + 2 * n_gridpoints;
-	double * sigma_t = E + 3 * n_gridpoints;
-
-	// Initialize E to log scale (10^-2 <-> 10^2)
-	printf("Initializing Log Scale...\n");
-	for( int i = 0; i < n_gridpoints; i++ )
+	for( int j = 0; j < nr; j++ )
 	{
-		double delta = 2.0 / n_gridpoints;
-		E[i] = pow(10.0,delta*i);
+		double r = 2603911.0 / R[j].Eo * (A+1) / A;
+		double q = 2.0 * sqrt(r * sigma_pot);
+		double T = R[j].Tn + R[j].Tg;
+		double x = 2.0 * (E - R[j].Eo) / T;
+		double xi = T * sqrt(A / (4.0 * k * temp * R[j].Eo));
+		double complex faddeeva_in = x + I;
+		faddeeva_in *= xi;
+		//double complex faddeeva_out = xi * FNF( faddeeva_in);
+		double complex faddeeva_out = xi * Faddeeva_w( faddeeva_in, 0.0);
+		double psi = sqrt(M_PI) * creal(faddeeva_out); 
+		double chi = sqrt(M_PI) * cimag(faddeeva_out);
+		xs.sigma_g += R[j].Tn * R[j].Tg / (T*T) * sqrt(R[j].Eo / E) * r * psi;
+		xs.sigma_n += R[j].Tn * R[j].Tn / (T*T) * ( r * psi + q * T/R[j].Tn * chi ); 
+	}
+	xs.sigma_n += sigma_pot;
+	xs.sigma_t = xs.sigma_g + xs.sigma_n;
+
+	return xs;
+}
+
+
+int main(void)
+{
+	int gp = 10000;
+	int nr;
+	Resonance * R = res_read(&nr);
+	XS * xs = (XS *) malloc( gp * sizeof(XS));
+	double * E = (double *) malloc( gp * sizeof(XS));
+
+	for( int i = 0; i < gp; i++ )
+	{
+		double delta = 4.0 / gp;
+		E[i] = pow(10.0, -2.0 + delta*i);
 	}
 
-	// Calculate sigmas
-	printf("Calculating XS's for %d energy levels using %d resonances...\n",
-			n_gridpoints, n_resonances);
-	for( int i = 0; i < n_gridpoints; i++ )
-	{
-		// Accumulate Contributions from Each Resonance
-		for( int j = 0; j < n_resonances; j++ )
-		{
-			double r = 2603911.0 / R[j].Eo * (A+1) / A;
-			double q = 2.0 * sqrt(r * sigma_pot);
-			double T = R[j].Tn + R[j].Tg;
-			double x = 2.0 * (E[i] - R[j].Eo) / T;
-			double psi, chi;
-			if( temperature_dependent )
-			{
-				double xi = T * sqrt(A / (4.0 * k * temp * R[j].Eo));
-				double complex faddeeva_in = x + I;
-				faddeeva_in *= xi;
-				//double complex faddeeva_out = xi * FNF( faddeeva_in);
-				double complex faddeeva_out = xi * Faddeeva_w( faddeeva_in, 0.0);
-				psi = sqrt(M_PI) * creal(faddeeva_out); 
-				chi = sqrt(M_PI) * cimag(faddeeva_out);
-			}
-			else
-			{
-				psi = 1.0 / (1.0 + x*x);
-				chi = x / (1.0 + x*x);
-			}
-			sigma_g[i] += R[j].Tn * R[j].Tg / (T*T) * sqrt(R[j].Eo / E[i]) * r * psi;
-			sigma_n[i] += R[j].Tn * R[j].Tn / (T*T) * ( r * psi + q * T/R[j].Tn * chi ); 
-		}
-		sigma_n[i] += sigma_pot;
+	for( int i = 0; i < gp; i++ )
+		xs[i] = calculate_XS(E[i], R, nr);
 
-		// Calculate Total XS
-		sigma_t[i] += sigma_g[i] + sigma_n[i];
-	}
-
-	// Save Data to File
-	printf("Printing output data to \"data.dat\"\n");
 	FILE * fp = fopen("data.dat", "w");
-	for( int i = 0; i < n_gridpoints; i++ )
+	for( int i = 0; i < gp; i++ )
 	{
 		fprintf(fp, "%e\t%e\t%e\t%e\n",
-				E[i],
-				sigma_g[i],
-				sigma_n[i],
-				sigma_t[i]);
+				xs[i].E,
+				xs[i].sigma_g,
+				xs[i].sigma_n,
+				xs[i].sigma_t);
 	}
 	fclose(fp);
 
@@ -122,7 +108,7 @@ Resonance * res_read(int * n_resonances)
 	rewind(fp);
 
 	Resonance * R = (Resonance *) malloc( lines * sizeof(Resonance));
-		
+
 	for( int i = 0; i < lines; i++ )
 	{
 		float dummy;
@@ -134,13 +120,6 @@ Resonance * res_read(int * n_resonances)
 				&R[i].Tg,
 				dum,
 				dum);
-		/*
-	printf("Resonance %d: Eo = %e\tTn = %e\t Tg = %e\n",
-			i,
-			R[i].Eo,
-			R[i].Tn,
-			R[i].Tg);
-			*/
 	}
 
 	fclose(fp);
